6dcf521c22eefd53c63d68b6fab48d7e69b6e3f4
Valfrido Sales <valfridosales@hotmail.com>
2025-03-28 14:34:41 -0400

    Sliders added

---
+++ Color Selector RGB/ColorSliderView.swift
@@ -0,0 +1,18 @@
+//
+//  ColorSliderView.swift
+//  Color Selector RGB
+//
+//  Created by valfrido sales on 2025-04-01.
+//
+
+import SwiftUI
+
+struct ColorSliderView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ColorSliderView()
+}
