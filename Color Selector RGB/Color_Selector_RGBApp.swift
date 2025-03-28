ff50bbcfcdea332372c328807fe86eee581fd90d
Valfrido Sales <valfridosales@hotmail.com>
2025-03-28 13:46:16 -0400

    Initial Commit

---
+++ Color Selector RGB/Color_Selector_RGBApp.swift
@@ -0,0 +1,17 @@
+//
+//  Color_Selector_RGBApp.swift
+//  Color Selector RGB
+//
+//  Created by valfrido sales on 2025-03-28.
+//
+
+import SwiftUI
+
+@main
+struct Color_Selector_RGBApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Color Selector RGBTests/Color_Selector_RGBTests.swift
@@ -0,0 +1,17 @@
+//
+//  Color_Selector_RGBTests.swift
+//  Color Selector RGBTests
+//
+//  Created by valfrido sales on 2025-03-28.
+//
+
+import Testing
+@testable import Color_Selector_RGB
+
+struct Color_Selector_RGBTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
